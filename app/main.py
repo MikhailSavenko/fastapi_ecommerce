49e2dd755603c77c49e7e5c55e9ed8ec80c3053c
Михаил Савенко <mi47sav@gmail.com>
2025-03-25 19:13:56 +0300

    log_middleware логирование запросов через промежуточное ПО + logger

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from loguru import logger
+from uuid import uuid4
+
 from app.routers import category
 from app.routers import products
 from app.routers import auth
@@ -7,6 +11,24 @@ from app.routers import reviews
 
 app = FastAPI()
 
+logger.add("info.log", format="Log: [{extra[log_id]}]:{time} - {level} - {message}", level="INFO", enqueue=True)
+
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info("Successfully accessed" + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
+        
 
 @app.get("/")
 async def welcome() -> dict:
@@ -18,3 +40,5 @@ app.include_router(products.router)
 app.include_router(auth.router)
 app.include_router(permission.router)
 app.include_router(reviews.router)
+
+
